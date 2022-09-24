9d1484012a3b1e439f6a05484e7cd40a66889c97
Lunannam <lunkova02@gmail.com>
2022-09-24 08:22:28 +0300

    add dz6

--- check.py
+++ check.py
@@ -1,14 +1,17 @@
-# def number():
-#     while True:
-#         try:
-#             num = int (input(' введите число: '))
-#             return num
-#         except(ValueError):
-#             print('вы ввели не число')
-# num = abs(number())
-import random
+def check_number():
+     while True:
+         try:
+             num = int (input(' введите число: '))
+             return num
+         except(ValueError):
+             print('вы ввели не число')
+num = abs(check_number())
+#check_number()
 
-def bot_step ():
-    number = random.randrange(1,28)
-    print(number)   
-bot_step()
+
+# import random
+# 
+# def bot_step ():
+#     number = random.randrange(1,28)
+#     print(number)   
+# bot_step()
